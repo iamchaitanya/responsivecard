37af895fb9d8d9fa109ccbfda3c894b796d3cfa1
chaitanya prakash <chaitanya26aug@gmail.com>
2019-11-03 16:42:59 +0530

    swiper over

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-var swiper = new Swiper('.swiper-container', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+  var swiper = new Swiper('.swiperjs', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
+var swiper = new Swiper('.swipertwo' , {
+  effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+});
