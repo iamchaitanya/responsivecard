78acc1b3dca6d5acacd0a2dd81df05a9d7a63893
chaitanya prakash <chaitanya26aug@gmail.com>
2019-11-13 23:15:34 +0530

    login

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-  var swiper = new Swiper('.swiperjs', {
+
+ var swiper = new Swiper('.swiperjs', {
     loop: true,
     autoplay: true,
     navigation: {
@@ -27,50 +28,47 @@ var swiper = new Swiper('.swipertwo' , {
 });
 
 
-// db.collection('tournament').get().then((snapshot) => {
-//   snapshot.docs.forEach(doc =>{
-//     console.log(doc.data());
-//   });
-  
-// });
-
-
- function add() {
-  // db.collection('tournament').get().then((snapshot) => {
-  //   snapshot.docs.forEach(doc =>{
-  //     console.log(doc.data());
-  //   });
-  
-  // });
-  console.log("object");
-  console.log("jai");
- };
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+var swiper = new Swiper('.swiperthree', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints:{
+    700:{
+      slidesPerView: 6,
+    }
   }
-} 
+});
+
+ 
+// //Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// } 
 
 
 // var test = document.getElementsByTagName("a");
@@ -82,9 +80,24 @@ window.onclick = function(event) {
 // };
 
 
-const cafeList = document.querySelector('#cafe-list');
 
-// function renderCafe(doc){
+  
 
-// }  
+function add() {
+  var docRef = db.collection("tournament").doc("20:00");
 
+ docRef.get().then((snapshot)=>{
+ var x = snapshot.data().seats;  
+ console.log(x);
+ x= x-1;
+ console.log(x);
+ docRef.update({
+   "seats": x,
+ })
+ })
+ 
+ };
+
+
+
+ 
